6b365fcfdbc80a8dd2bd9dc4a87ef52741419bfb
jam4jm <107881114+jam4jm@users.noreply.github.com>
2022-08-14 17:34:06 +0300

    unit conversion
    added unit temperature conversion to fahrenheit

--- src/index.js
+++ src/index.js
@@ -17,12 +17,7 @@ function timeFormat() {
   let time = document.querySelector("#time");
   time.innerHTML = `${weekday}, ${hours}:${minutes}`;
 }
-
-function citySearch(event) {
-  event.preventDefault();
-  let input = document.querySelector("#citySearch");
-  let inputCity = input.value;
-  inputCity = inputCity.trim().toLowerCase();
+function citySearch(inputCity) {
   let apiKey = "5d9e2b27efed7d8f99a351a14fe276b8";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=${apiKey}&units=metric`;
   function cityInput(response) {
@@ -33,10 +28,18 @@ function citySearch(event) {
   axios.get(apiUrl).then(nowTemperature);
 }
 
+function formSubmіt(event) {
+  event.preventDefault();
+  let input = document.querySelector("#citySearch");
+  let inputCity = input.value;
+  inputCity = inputCity.trim().toLowerCase();
+  citySearch(inputCity);
+}
+
 function nowTemperature(response) {
   let now = Math.round(response.data.main.temp);
   let nowTemperature = document.querySelector("#nowTemperature");
-  nowTemperature.innerHTML = `${now}°C`;
+  nowTemperature.innerHTML = `${now}`;
   let nowType = document.querySelector("#nowType");
   nowType.innerHTML = response.data.weather[0].main;
   let humidity = Math.round(response.data.main.humidity);
@@ -51,6 +54,9 @@ function nowTemperature(response) {
   let icon = response.data.weather[0].icon;
   let nowWeatherIcon = document.querySelector("#todayWeatherIcon");
   nowWeatherIcon.setAttribute("src", `img/${icon}.png`);
+  let unit = document.querySelector("#unit");
+  unit.innerHTML = "°C";
+  celsius = response.data.main.temp;
 }
 function currentPosition(position) {
   let lat = position.coords.latitude;
@@ -67,7 +73,33 @@ function currentPosition(position) {
   }
   return currentWeather();
 }
+function fahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureValue = document.querySelector("#nowTemperature");
+  let fahrenheit = Math.round(celsius * 1.8 + 32);
+  let unit = document.querySelector("#unit");
+  temperatureValue.innerHTML = fahrenheit;
+  unit.innerHTML = "°F";
+  celsiusDisplay.classList.remove("active");
+  fahrenheitDisplay.classList.add("active");
+}
+function celsiusTemperature(event) {
+  event.preventDefault();
+  let temperatureValue = document.querySelector("#nowTemperature");
+  let unit = document.querySelector("#unit");
+  temperatureValue.innerHTML = Math.round(celsius);
+  unit.innerHTML = "°C";
+  celsiusDisplay.classList.add("active");
+  fahrenheitDisplay.classList.remove("active");
+}
 timeFormat();
+citySearch("mariupol");
 navigator.geolocation.getCurrentPosition(currentPosition);
 let cityForm = document.querySelector("#cityForm");
-cityForm.addEventListener("submit", citySearch);
+cityForm.addEventListener("submit", formSubmіt);
+
+let celsius = null;
+let fahrenheitDisplay = document.querySelector("#fahrenheit");
+fahrenheitDisplay.addEventListener("click", fahrenheitTemperature);
+let celsiusDisplay = document.querySelector("#celsius");
+celsiusDisplay.addEventListener("click", celsiusTemperature);
