0deef45fd938a6fd888643ec467f94f42e0e10f4
jam4jm <107881114+jam4jm@users.noreply.github.com>
2022-08-20 16:14:36 +0300

    forecast added
    added max and min temperature for current day
    4-day forecast

--- src/index.js
+++ src/index.js
@@ -17,6 +17,14 @@ function timeFormat() {
   let time = document.querySelector("#time");
   time.innerHTML = `${weekday}, ${hours}:${minutes}`;
 }
+function dayFormat(time) {
+  let date = new Date(time * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = date.getDay();
+  let dayDate = date.getDate();
+  dayDate = dayDate.toString().padStart(2, "0");
+  return `${days[day]} ${dayDate}`;
+}
 function citySearch(inputCity) {
   let apiKey = "5d9e2b27efed7d8f99a351a14fe276b8";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=${apiKey}&units=metric`;
@@ -54,6 +62,7 @@ function nowTemperature(response) {
   let icon = response.data.weather[0].icon;
   let nowWeatherIcon = document.querySelector("#todayWeatherIcon");
   nowWeatherIcon.setAttribute("src", `img/${icon}.png`);
+  forecast(response.data.coord);
 }
 function currentPosition(position) {
   let lat = position.coords.latitude;
@@ -71,8 +80,46 @@ function currentPosition(position) {
   return currentWeather();
 }
 
+function forecast(coordinates) {
+  let apiKey = "5d9e2b27efed7d8f99a351a14fe276b8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(forecastTemperature);
+}
+function forecastTemperature(response) {
+  let forecast = response.data.daily;
+  let todayMaxTemperature = Math.round(forecast[0].temp.max);
+  let maxTemperature = document.querySelector("#maxTemperature");
+  maxTemperature.innerHTML = `${todayMaxTemperature}°C`;
+  let todayMinTemperature = Math.round(forecast[0].temp.min);
+  let minTemperature = document.querySelector("#minTemperature");
+  minTemperature.innerHTML = `${todayMinTemperature}°C`;
+  let dayForecast = `<div class="row dayForecast" id="dayForecast">`;
+  forecast.forEach(function eachDay(forecastDay, index) {
+    if (index >= 1 && index <= 4) {
+      dayForecast =
+        dayForecast +
+        `<div class="col-sm-3 dayForecast">
+              <div class="date">${dayFormat(forecastDay.dt)}</div>
+              <div class="maxDayTemperature">${Math.round(
+                forecastDay.temp.max
+              )}°C</div>
+              <div class="minDayTemperature">${Math.round(
+                forecastDay.temp.min
+              )}°C</div>
+              <img class="forecastWeatherIcon" src="img/${
+                forecastDay.weather[0].icon
+              }.png" />
+            </div>
+          `;
+    }
+  });
+  dayForecast = dayForecast + `</div>`;
+  let dailyForecast = document.querySelector("#dayForecast");
+  dailyForecast.innerHTML = dayForecast;
+}
+
 timeFormat();
-citySearch("mariupol");
+citySearch("kyiv");
 navigator.geolocation.getCurrentPosition(currentPosition);
 let cityForm = document.querySelector("#cityForm");
 cityForm.addEventListener("submit", formSubmіt);
